0a9918150ff8a8735293c87d91c1f654b936f66b
seli <selisinha22@gmail.com>
2023-05-27 20:09:26 +0530

    commit #3 profile-login

--- app/src/main/java/eu/tutorials/cyberwatch/ProfileActivity.kt
+++ app/src/main/java/eu/tutorials/cyberwatch/ProfileActivity.kt
@@ -1,11 +1,25 @@
 package eu.tutorials.cyberwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null) {
+            // User is not logged in, redirect to login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the current activity if not needed
+        }
     }
 }
