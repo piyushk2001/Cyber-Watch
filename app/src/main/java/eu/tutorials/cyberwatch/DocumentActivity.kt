0b675edf41c9369c264b67c3dfe95da41d77303e
seli <selisinha22@gmail.com>
2023-07-08 17:06:10 +0530

    upload pfp and display but not constant

--- app/src/main/java/eu/tutorials/cyberwatch/DocumentActivity.kt
+++ app/src/main/java/eu/tutorials/cyberwatch/DocumentActivity.kt
@@ -1,11 +1,78 @@
 package eu.tutorials.cyberwatch
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class DocumentActivity : AppCompatActivity() {
+    private lateinit var registerBtn: Button
+    private lateinit var uploadBtn: Button
+    // creating a storage reference
+    private var storageRef = Firebase.storage.reference
+
+    private var imagePickerActivityResult: ActivityResultLauncher<Intent> =
+    // lambda expression to receive a result back, here we
+        // receive single item(photo) on selection
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // getting URI of selected Image
+                val imageUri: Uri? = result.data?.data
+
+                // extract the file name with extension
+                val sd = getFileName(applicationContext, imageUri!!)
+
+                // Upload Task with upload to directory 'file'
+                // and name of the file remains same
+                val uploadTask = storageRef.child("file/$sd").putFile(imageUri)
+
+                // On success, show toast message
+                uploadTask.addOnSuccessListener {
+                    Toast.makeText(this, "Successfully uploaded", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Log.e("Firebase", "Image Upload fail")
+                }
+            }
+        }
+
+    private fun getFileName(context: Context, uri: Uri): String? {
+        var fileName: String? = null
+        uri.path?.let { path ->
+            val file = File(path)
+            fileName = file.name
+        }
+        return fileName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document)
+
+        registerBtn = findViewById(R.id.registerBtn)
+        registerBtn.setOnClickListener(View.OnClickListener {
+            //FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
+        uploadBtn = findViewById(R.id.uploadBtn)
+        uploadBtn.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK)
+            galleryIntent.type = "image/*"
+            imagePickerActivityResult.launch(galleryIntent)
+        }
     }
 }
+
--- app/src/main/java/eu/tutorials/cyberwatch/ProfileActivity.kt
+++ app/src/main/java/eu/tutorials/cyberwatch/ProfileActivity.kt
@@ -1,17 +1,24 @@
 package eu.tutorials.cyberwatch
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private var auth: FirebaseAuth= FirebaseAuth.getInstance()
     private lateinit var firestore: FirebaseFirestore
     private lateinit var userNameTextView: TextView
     private lateinit var addressTextView: TextView
@@ -19,17 +26,35 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var emailTextView: TextView
     private lateinit var signOutBtn: Button
     private lateinit var editProfileBtn: ImageButton
+    private lateinit var uploadBtn: ImageView
+    private lateinit var imageview: ImageView
+
+    private var storageRef = Firebase.storage.reference
+    private var userEmail = auth.currentUser!!.email
+    //private var imageview:ImageView= findViewById(R.id.imageView3)
+
+
+
+    private fun getFileName(context: Context, uri: Uri): String? {
+        var fileName: String? = null
+        uri.path?.let { path ->
+            val file = File(path)
+            fileName = file.name
+        }
+        return fileName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        auth = FirebaseAuth.getInstance()
+
         firestore = FirebaseFirestore.getInstance()
         userNameTextView = findViewById(R.id.userName)
         addressTextView = findViewById(R.id.userAddress)
         phoneTextView = findViewById(R.id.userPhone)
         emailTextView = findViewById(R.id.userEmail)
+        imageview = findViewById(R.id.imageView3)
 
 
         if (auth.currentUser == null) {
@@ -53,6 +78,38 @@ class ProfileActivity : AppCompatActivity() {
             //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startEditActivity()
         })
+        var imagePickerActivityResult: ActivityResultLauncher<Intent> =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val imageUri: Uri? = result.data?.data
+
+                    val sd = getFileName(applicationContext, imageUri!!)
+
+                    val uploadTask = storageRef.child("ProfilePictures/$userEmail").putFile(imageUri)
+
+                    // On success, download the file URL and display it
+                    uploadTask.addOnSuccessListener {
+                        // using glide library to display the image
+                        storageRef.child("ProfilePictures/$userEmail").downloadUrl.addOnSuccessListener { downloadUrl ->
+                            Glide.with(this)
+                                .load(downloadUrl)
+                                .into(imageview)
+
+                            Log.e("Firebase", "download passed")
+                        }.addOnFailureListener {
+                            Log.e("Firebase", "Failed in downloading")
+                        }
+                    }.addOnFailureListener {
+                        Log.e("Firebase", "Image Upload fail")
+                    }
+                }
+            }
+        uploadBtn = findViewById(R.id.uploadBtn)
+        uploadBtn.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK)
+            galleryIntent.type = "image/*"
+            imagePickerActivityResult.launch(galleryIntent)
+        }
 
     }
 
@@ -65,6 +122,7 @@ class ProfileActivity : AppCompatActivity() {
         if (email != null) {
             val userRef = firestore.collection("User").document(email)
             userRef.get()
+
                 .addOnSuccessListener { documentSnapshot ->
                     if (documentSnapshot.exists()) {
                         val name = documentSnapshot.getString("name")
